2e4494229e2cfbb97f0cf15645b8edfd74eddd25
spaceneedle <spaceneedle@gmail.com>
2015-03-30 14:22:22 -0700

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,16 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Casey on 3/10/15.
+//  Copyright (c) 2015 Casey Halverson. All rights reserved.
+//
+
+import Foundation
+
+// class that contains our audio file information
+
+class RecordedAudio: NSObject {
+    var filePathUrl: NSURL!
+    var title: String!
+}
